ef739ad7ccf3dddcbfda2adfead3694f0771a980
Noah Stewart <noah.stewart9813@gmail.com>
2019-06-09 21:53:42 -0400

    finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home' => 'static_pages#home'
-  get 'static_pages/help' => 'static_pages#help'
-  get 'static_pages/about' => 'static_pages#about'
-  get 'static_pages/contact' => 'static_pages#contact'
+  get 'users/new'
+  root 'static_pages#home' # root_path = '/'; root_url = ... .com or .com/ get defined automatically
+  get '/help' => 'static_pages#help' # help_path = '/help'; help_url = ... .com/help get defined automatically
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
+  get '/signup' => 'users#new' #signup_path = '/signup'
 end
